ac1c57242e496285d15b5b569ffa369a9c7dc924
German Ulrich <gerulrich@gmail.com>
2022-11-18 10:16:25 -0300

    add some coments

--- scripts/env.sh
+++ scripts/env.sh
@@ -4,6 +4,8 @@
 # Generate an RSA ssh key pair with 4096 bits using the command
 # $ ssh-keygen -t rsa -b 4096
 #
+# Copy public key in remote host 
+# OpenWRT: System -> Administration -> SSH-Keys Tab.
 
 ROUTER_IP="192.168.0.2"
 KEY_FILE=~/id_rsa_tplink
