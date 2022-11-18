d81421acf6715591375f0bd62209f19e42fb5e67
German Ulrich <gerulrich@gmail.com>
2022-11-18 10:24:14 -0300

    validate if device is disable before enable/disable internet

--- scripts/disable_internet.sh
+++ scripts/disable_internet.sh
@@ -3,4 +3,8 @@
 DIR=$(dirname $0)
 . $DIR/env.sh
 
-ssh -i ${KEY_FILE} root@${ROUTER_IP} "iptables -I FORWARD -s $1 -j DROP"
+disabled=$($DIR/firewall_list.sh | grep $1 | wc -l)
+
+if [ $disabled -eq 0 ]; then
+    ssh -i ${KEY_FILE} root@${ROUTER_IP} "iptables -I FORWARD -s $1 -j DROP"
+fi
--- scripts/enable_internet.sh
+++ scripts/enable_internet.sh
@@ -3,4 +3,8 @@
 DIR=$(dirname $0)
 . $DIR/env.sh
 
-ssh -i ${KEY_FILE} root@${ROUTER_IP} "iptables -D FORWARD -s $1 -j DROP"
+disabled=$($DIR/firewall_list.sh | grep $1 | wc -l)
+
+if [ $disabled -gt 0 ]; then
+    ssh -i ${KEY_FILE} root@${ROUTER_IP} "iptables -D FORWARD -s $1 -j DROP"
+fi
